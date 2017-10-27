d59e9393aa1910e99825633bf0b030ee6eed554e
Jukelips <adrien.mchl@gmail.com>
2017-10-27 13:22:27 +0200

    Commit des components nécessaire pour le projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
+import { LoginComponent } from './login/login.component';
+import { ServicesComponent } from './services/services.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
+import { CompteComponent } from './compte/compte.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    LoginComponent,
+    ServicesComponent,
+    RegisterComponent,
+    LogoutComponent,
+    CompteComponent
   ],
   imports: [
     BrowserModule, FormsModule
