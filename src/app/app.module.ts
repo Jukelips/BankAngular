40b09dfd59cbecd3f648ae41aeba5bcffa5f6604
Sylrus <sylrus78@gmail.com>
2017-10-30 14:37:35 +0100

    Front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes ,provideRoutes} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -8,6 +9,17 @@ import { ServicesComponent } from './services/services.component';
 import { RegisterComponent } from './register/register.component';
 import { LogoutComponent } from './logout/logout.component';
 import { CompteComponent } from './compte/compte.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes: Routes = [
+  { path: '',  redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'logout', component: LoginComponent },
+  { path: 'compte', component: CompteComponent },
+  { path: 'services', component: ServicesComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,10 +29,12 @@ import { CompteComponent } from './compte/compte.component';
     ServicesComponent,
     RegisterComponent,
     LogoutComponent,
-    CompteComponent
+    CompteComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
